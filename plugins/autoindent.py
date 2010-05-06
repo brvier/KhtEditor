3a957b0f54ca554dc2212a1daf954f54ff007ed1
Benoît HERVIER <khertan@khertan.net>
2010-05-06 23:19:39 +0200

    Small prototype

---
+++ plugins/autoindent.py
@@ -0,0 +1,22 @@
+from PyQt4.QtGui import QTextEdit
+from PyQt4.QtCore import Qt
+from plugins import Plugin
+import re
+
+class AutoIndent_Plugin(Plugin):
+    capabilities = ['afterKeyPressEvent']
+
+    def do_afterKeyPressEvent(self, widget,event):
+        if (event.key() == Qt.Key_Return) or (event.key() == Qt.Key_Enter):
+            # copy whitespace from the beginning of the previous line
+            cursor = widget.textCursor()
+            block = widget.document().findBlockByNumber(cursor.blockNumber()-1)
+            whitespace = re.match(r"(\s*)", str(block.text().toUtf8())).group(1)
+#            event.ignore()
+#            QTextEdit.keyPressEvent(widget, event)
+            cursor = widget.textCursor()
+            format = cursor.blockFormat()
+            format.clearBackground()
+            cursor.setBlockFormat(format)
+            cursor.insertText(whitespace)
+#            print 'marche pas'
