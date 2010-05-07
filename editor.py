94cc544c9dabeb3f6624509018a62afe68451205
Benoît HERVIER <khertan@khertan.net>
2010-05-07 07:58:09 +0200

    Cleaner separation of class in files

--- editor.py
+++ editor.py
@@ -1,216 +1,23 @@
-class Configurable(object):
-    
-    
-    def __init__(self):
-        self.connect(self, SIGNAL("settings_changed()"), self.on_reconfigure)
+#!/usr/bin/env python
+
+"""KhtEditor a source code editor by Khertan : Code Editor"""
+
+import sys
+import re
+from PyQt4 import QtCore, QtGui
+from plugins import init_plugin_system, get_plugins_by_capability
+
+class Kht_Editor(QtGui.QTextEdit):
+    def __init__(self, parent=None):
+        QtGui.QTextEdit.__init__(self)
+        init_plugin_system({'plugin_path': './plugins', 'plugins': ['autoindent']})
 
 
-    def reconfigure(self, **settings):
-        try:
-            self.settings.update(settings)
-        except AttributeError, e:
-            try:
-                self.settings = jdict(self.default_settings)
-            except AttributeError, e:
-                self.settings = jdict()
-            self.settings.update(settings)
-        self.emit(SIGNAL("settings_changed()"))
-
-
-    def on_reconfigure(self):
-        pass
-
-
-class Hookable(object):
-    
-    
-    def setup_hooks(self):
-        self.state = 0
-        self.commands = {}
-        self.keyboard_hooks = {}
-        self.mouse_press_hooks = {}
-        self.mouse_double_click_hooks = {}
-        self.paint_hooks = self.paint_hooks_before = {}
-        self.paint_hooks_after = {}
-        self.resize_hooks = {}
-        self.contents_change_hooks = {}
-        self.cursor_movement_hooks = {}
-        self.row_change_hooks = {}
-        # These variables fill be filled with hooks for current state
-        self._commands = []
-        self.active_paint_hooks_before = self.active_paint_hooks_after = ()
-        self.active_resize_hooks = ()
-        self.active_cursor_movement_hooks = ()
-        self.active_row_change_hooks = ()
-
-        
-        self.state = -1
-        self.installed_modes = []
-
-        
-    def prepare_hooks(self):
-        for cmd in self._commands:
-            self.removeAction(cmd)
-
-        self._commands = self.commands.get(self.state, [])
-        for cmd in self._commands:
-            self.addAction(cmd)
-
-        def filter_active_state(hooks):
-            return list(hooks.get(self.state, []) + hooks.get(-1, []))
-
-        self._mouse_press_hooks = filter_active_state(self.mouse_press_hooks)
-        
-        self._mouse_double_click_hooks = filter_active_state(self.mouse_double_click_hooks)
-        self._paint_hooks_before = filter_active_state(self.paint_hooks_before)
-        self._paint_hooks_after = filter_active_state(self.paint_hooks_after)
-        self._contents_change_hooks = filter_active_state(self.contents_change_hooks)
-        self._cursor_movement_hooks = filter_active_state(self.cursor_movement_hooks)
-        self._row_change_hooks = filter_active_state(self.row_change_hooks)
-        self._resize_hooks = filter_active_state(self.resize_hooks)
-
-        h = self._keyboard_hooks = dict()
-        for state in (self.state, -1):
-            for keys, callbacks in self.keyboard_hooks.get(state, {}).items():
-                h.setdefault(keys, []).extend(callbacks)
-   
-
-    def find_mode(self, cls):
-        for mode in self.installed_modes:
-            if isinstance(mode, cls):
-                return mode
-
-
-    def is_installed(self, mode):
-        for installed in self.installed_modes:
-            if installed.__class__ is mode:
-                return True
-        return False
-
-
-    def _find_modes(self, *mode_args):
-        for m in mode_args:
-            mode = ModeBase.find_implementation(m)
-            if not mode:
-                raise ValueError, "Installing mode %r failed: Not found." % m
-            yield mode
-
-
-    def install(self, *mode_list):
-        for mode in self._find_modes(*mode_list):
-            if not self.is_installed(mode):
-                mode_instance = mode(self)
-                self.installed_modes.append(mode_instance)
-                setattr(self, mode.__name__, mode_instance)
-        self.switch_state(0)
-
-    require = install #alias for emacs fans ;)
-
-    
-    def install_on_idle(self, *mode_list, **kwargs):
-        callback = kwargs.pop("callback", None)
-        self.idle_install = IdleCall(self, self.install, callback, *mode_list, **kwargs)
-
-
-    def uninstall(self, *mode_list):
-        for mode in self._find_modes(*mode_list):
-            for instance in self.installed_modes:
-                if isinstance(instance, mode):
-                    instance.uninstall()
-                    delattr(self, instance.__class__.__name__)
-                    self.installed_modes.remove(instance)
-                    break
-
-
-    def switch_state(self, state):
-        """
-        changes the state and set hooks for current state
-        """
-        self.emit(SIGNAL("state_change(int)"), state)
-        self.state = state
-        self.prepare_hooks()
-        self.update_hooks()
-        self.emit(SIGNAL("new_state()"))
-
-class Buffer(QTextDocument, Hookable, Configurable):
-
-    killring = []
-    registers = []
-    instances =  []
-
-
-    @classmethod
-    def get(cls, idx):
-        if isinstance(idx, basestring):
-            for buf in cls.instances:
-                if buf.filename == idx:
-                    return bux
-        return cls.instances[idx]
-
-
-    def __init__(self, text=None, filename=None):
-        QTextDocument.__init__(self)
-        Hookable.__init__(self)
-        Configurable.__init__(self)
-        self.filename = filename
-        if text is None and filename and os.path.exists(filename):
-            text = open(filename).read()
-        self.plain_text_layout = QPlainTextDocumentLayout(self)
-        self.setDocumentLayout(self.plain_text_layout)
-        if text:
-            self.setPlainText(text)
-        self.setModified(False)
-        Buffer.instances.append(self)
-        self.setup_hooks()
-        #self.install("Autosave")
-
-        
-    def document(self):
-        return self # work-around...
-
-
-    def close(self):
-        Buffer.instances.remove(self)
-
-
-    def max_line_length(self):
-        lines = unicode(self.toPlainText()).splitlines()
-        return max([len(l) for l in lines])
-
-
-    def row_col2position(self, row, col):
-        """
-        index starts with 0
-        """
-        block = self.findBlockByNumber(row)
-        return block.position() + col
-
-
-    def row2position(self, row):
-        return self.row_col2position(row, 0)
-
-
-class Idler(QObject):
-    # XXX make seconds configurable?
-
-
-    def __init__(self, textedit):
-        QObject.__init__(self, textedit)
-        self.textedit = textedit
-        self.idle_timer_id = self.startTimer(1000)
-
-
-    def busy(self):
-        self.killTimer(self.idle_timer_id)
-        self.idle_timer_id = self.startTimer(1000)
-
-
-    def timerEvent(self, event):
-        self.killTimer(self.idle_timer_id)
-        QTimer.singleShot(0, self._emit_idle)
-        self.idle_timer_id = self.startTimer(1000)
-
-
-    def _emit_idle(self):
-        self.textedit.emit(SIGNAL("idle()"))
-        
+    def keyPressEvent(self, e):
+        for plugin in get_plugins_by_capability('beforeKeyPressEvent'):
+            plg = plugin()
+            plg.do_beforeKeyPressEvent(self,e)
+        QtGui.QTextEdit.keyPressEvent(self, e)
+        for plugin in get_plugins_by_capability('afterKeyPressEvent'):
+            plg = plugin()
+            plg.do_afterKeyPressEvent(self,e)
---
+++ syntax/python_highlighter.py
@@ -0,0 +1,298 @@
+#!/usr/bin/env python
+
+"""KhtEditor a source code editor by Khertan : Python Syntax Hilighter"""
+
+import sys
+import re
+from PyQt4 import QtCore, QtGui
+
+class Highlighter(QtGui.QSyntaxHighlighter):
+
+    preprocessors = ['import',
+        'from',
+        'as',
+        'False',
+        'None',
+        'True',
+        '__name__',
+        '__debug__'
+    ]
+
+    keywords = ['and',
+        'assert',
+        'break',
+        'continue',
+        'del',
+        'elif',
+        'else',
+        'except',
+        'exec',
+        'finally',
+        'for',
+        'global',
+        'if',
+        'in',
+        'is',
+        'lambda',
+        'not',
+        'or',
+        'pass',
+        'print',
+        'raise',
+        'try',
+        'while',
+        'yield',
+                'def',
+                'class',
+                'return',
+    ]
+
+    specials = ['ArithmeticError',
+        'AssertionError',
+        'AttributeError',
+        'EnvironmentError',
+        'EOFError',
+        'Exception',
+        'FloatingPointError',
+        'ImportError',
+        'IndentationError',
+        'IndexError',
+        'IOError',
+        'KeyboardInterrupt',
+        'KeyError',
+        'LookupError',
+        'MemoryError',
+        'NameError',
+        'NotImplementedError',
+        'OSError',
+        'OverflowError',
+        'ReferenceError',
+        'RuntimeError',
+        'StandardError',
+        'StopIteration',
+        'SyntaxError',
+        'SystemError',
+        'SystemExit',
+        'TabError',
+        'TypeError',
+        'UnboundLocalError',
+        'UnicodeDecodeError',
+        'UnicodeEncodeError',
+        'UnicodeError',
+        'UnicodeTranslateError',
+        'ValueError',
+        'WindowsError',
+        'ZeroDivisionError',
+
+        'Warning',
+        'UserWarning',
+        'DeprecationWarning',
+        'PendingDeprecationWarning',
+        'SyntaxWarning',
+        'OverflowWarning',
+        'RuntimeWarning',
+        'FutureWarning',
+
+        '__import__',
+        'abs',
+        'apply',
+        'basestring',
+        'bool',
+        'buffer',
+        'callable',
+        'chr',
+        'classmethod',
+        'cmp',
+        'coerce',
+        'compile',
+        'complex',
+        'delattr',
+        'dict',
+        'dir',
+        'divmod',
+        'enumerate',
+        'eval',
+        'execfile',
+        'file',
+        'filter',
+        'float',
+        'getattr',
+        'globals',
+        'hasattr',
+        'hash',
+        'hex',
+        'id',
+        'input',
+        'int',
+        'intern',
+        'isinstance',
+        'issubclass',
+        'iter',
+        'len',
+        'list',
+        'locals',
+        'long',
+        'map',
+        'max',
+        'min',
+        'object',
+        'oct',
+        'open',
+        'ord',
+        'pow',
+        'property',
+        'range',
+        'raw_input',
+        'reduce',
+        'reload',
+        'repr',
+        'round',
+        'setattr',
+        'slice',
+        'staticmethod',
+        'str',
+        'sum',
+        'super',
+        'tuple',
+        'type',
+        'unichr',
+        'unicode',
+        'vars',
+        'xrange',
+        'zip',
+    ]
+
+    operators = [
+                '=',
+                 # Comparison
+                '==', '!=', '<', '<=', '>', '>=',
+                # Arithmetic
+                '\+', '-', '\*', '/', '//', '\%', '\*\*',
+                # In-place
+                '\+=', '-=', '\*=', '/=', '\%=',
+                # Bitwise
+                '\^', '\|', '\&', '\~', '>>', '<<',
+    ]
+
+    # Python braces
+    braces = [
+        '\{', '\}', '\(', '\)', '\[', '\]',
+    ]
+
+    def __init__(self, document):
+        super(Highlighter, self).__init__(document)
+        STYLES = {
+        'preprocessor': self.format('darkMagenta'),
+        'keyword': self.format('orange'),
+        'special': self.format('darkMagenta'),
+        'operator': self.format('darkMagenta'),
+        'brace': self.format('darkGray'),
+        'defclass': self.format('blue'),
+        'string': self.format('green'),
+        'string2': self.format('green'),
+        'comment': self.format('red'),
+        }
+
+        # Multi-line strings (expression, flag, style)
+        # FIXME: The triple-quotes in these two lines will mess up the
+        # syntax highlighting from this point onward
+        self.tri_single = (QtCore.QRegExp("'''"), 1, STYLES['string2'])
+        self.tri_double = (QtCore.QRegExp('"""'), 2, STYLES['string2'])
+
+        rules = []
+        rules += [(r'\b%s\b' % w, 0, STYLES['keyword']) for w in Highlighter.keywords]
+        rules += [(r'\b%s\b' % w, 0, STYLES['preprocessor']) for w in Highlighter.preprocessors]
+        rules += [(r'\b%s\b' % w, 0, STYLES['special']) for w in Highlighter.specials]
+        rules += [(r'%s' % o, 0, STYLES['operator']) for o in Highlighter.operators]
+        rules += [(r'%s' % b, 0, STYLES['brace']) for b in Highlighter.braces]
+        rules += [
+                # Double-quoted string, possibly containing escape sequences
+                (r'"[^"\\]*(\\.[^"\\]*)*"', 0, STYLES['string']),
+                # Single-quoted string, possibly containing escape sequences
+                (r"'[^'\\]*(\\.[^'\\]*)*'", 0, STYLES['string']),
+                # 'def' followed by an identifier
+                (r'\bdef\b\s*(\w+)', 1, STYLES['defclass']),
+                # 'class' followed by an identifier
+                (r'\bclass\b\s*(\w+)', 1, STYLES['defclass']),
+                # From '#' until a newline
+                (r'#[^\n]*', 0, STYLES['comment']),
+        ]
+
+        # Build a QRegExp for each pattern
+        self.rules = [(QtCore.QRegExp(pat), index, fmt) for (pat, index, fmt) in rules]
+
+    def format(self,color, style=''):
+        """Return a QTextCharFormat with the given attributes.
+        """
+        _color = QtGui.QColor()
+        _color.setNamedColor(color)
+
+        _format = QtGui.QTextCharFormat()
+        _format.setForeground(_color)
+        if 'bold' in style:
+            _format.setFontWeight(QFont.Bold)
+        if 'italic' in style:
+            _format.setFontItalic(True)
+
+        return _format
+
+    def highlightBlock(self, text):
+        """Apply syntax highlighting to the given block of text.
+        """
+        # Do other syntax formatting
+        for expression, nth, format in self.rules:
+            index = expression.indexIn(text, 0)
+
+            while index >= 0:
+                # We actually want the index of the nth match
+                index = expression.pos(nth)
+                length = expression.cap(nth).length()
+                self.setFormat(index, length, format)
+                index = expression.indexIn(text, index + length)
+
+            self.setCurrentBlockState(0)
+
+            # Do multi-line strings
+            in_multiline = self.match_multiline(text, *self.tri_single)
+            if not in_multiline:
+                in_multiline = self.match_multiline(text, *self.tri_double)
+
+    def match_multiline(self, text, delimiter, in_state, style):
+        """Do highlighting of multi-line strings. ``delimiter`` should be a
+        ``QRegExp`` for triple-single-quotes or triple-double-quotes, and
+        ``in_state`` should be a unique integer to represent the corresponding
+        state changes when inside those strings. Returns True if we're still
+        inside a multi-line string when this function is finished.
+        """
+        # If inside triple-single quotes, start at 0
+        if self.previousBlockState() == in_state:
+            start = 0
+            add = 0
+        # Otherwise, look for the delimiter on this line
+        else:
+            start = delimiter.indexIn(text)
+            # Move past this match
+            add = delimiter.matchedLength()
+
+        # As long as there's a delimiter match on this line...
+        while start >= 0:
+            # Look for the ending delimiter
+            end = delimiter.indexIn(text, start + add)
+            # Ending delimiter on this line?
+            if end >= add:
+                length = end - start + add + delimiter.matchedLength()
+                self.setCurrentBlockState(0)
+            # No; multi-line string
+            else:
+                self.setCurrentBlockState(in_state)
+                length = text.length() - start + add
+            # Apply formatting
+            self.setFormat(start, length, style)
+            # Look for the next match
+            start = delimiter.indexIn(text, start + length)
+
+        # Return True if still inside a multi-line string, False otherwise
+        if self.currentBlockState() == in_state:
+            return True
+        else:
+            return False
