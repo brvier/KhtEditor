e38ee3a8ed2e12939c0efdee814b4b568e6099c4
user <user@Nokia-N900.(none)>
2010-05-26 18:08:04 +0200

    Fix autoindent pluggin :

--- plugins/autoindent.py
+++ plugins/autoindent.py
@@ -12,8 +12,9 @@ class AutoIndent_Plugin(Plugin):
             cursor = widget.textCursor()
             block = widget.document().findBlockByNumber(cursor.blockNumber()-1)
             whitespace = re.match(r"(\s*)", str(block.text().toUtf8())).group(1)
-            if block.text().toUtf8().endswith(':'):
-                whitespace = whitespace + 4
+            print type(block.text().toUtf8()),dir(block.text().toUtf8())
+            if block.text().toUtf8().right(1) == ':':
+                whitespace = whitespace + '    '
             cursor = widget.textCursor()
             format = cursor.blockFormat()
             format.clearBackground()
