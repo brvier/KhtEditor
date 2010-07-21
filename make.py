c201d78b665b448f0feed99ab3730e5f63de0d8f
root <root@Nokia-N900.(none)>
2010-07-21 17:31:35 +0200

    Release 0.0.1

--- make.py
+++ make.py
@@ -35,56 +35,31 @@ if __name__ == "__main__":
     p.distribution="fremantle"
     p.repository="extras-devel"
     p.icon='khteditor.png'
-    p["/usr/bin"] = ["khteditor",]
+    p["/usr/bin"] = ["khteditor_launch.py",]
     p["/usr/share/dbus-1/services"] = ["khteditor.service",]
     p["/usr/share/pixmaps"] = ["khteditor.png",]
     p["/usr/share/applications/hildon"] = ["khteditor.desktop",]
-    files = [ "khteditor.py",
-                              "editor.py",
-                              "editor_window.py",
-                              "plugins.py",
-                              "main.py",
-                              "welcome_window.py",
-                              "recent_files.py",
-                              "snippets.py",
-                              ]
-
-    #Syntax
-    for root, dirs, fs in os.walk('/home/user/MyDocs/Projects/khteditor/syntax'):
+    files = []
+    
+    #Src
+    for root, dirs, fs in os.walk('/home/user/MyDocs/Projects/khteditor/khteditor'):
       for f in fs:
-        print os.path.basename(f)
-        files.append('syntax/'+os.path.basename(f))
+        #print os.path.basename(root),dirs,f
+        prefix = 'khteditor/'
+        if os.path.basename(root) != 'khteditor':
+            prefix = prefix + os.path.basename(root) + '/'
+        files.append(prefix+os.path.basename(f))
     print files
 
-    #Plugins
-    for root, dirs, fs in os.walk('/home/user/MyDocs/Projects/khteditor/plugins'):
-      for f in fs:
-        print os.path.basename(f)
-        files.append('plugins/'+os.path.basename(f))
-    print files
-
-    #Snippets
-    for root, dirs, fs in os.walk('/home/user/MyDocs/Projects/khteditor/snippets'):
-      for f in fs:
-        print os.path.basename(f)
-        files.append('snippets/'+os.path.basename(f))
-    print files
-
-    #Icon
-    for root, dirs, fs in os.walk('/home/user/MyDocs/Projects/khteditor/icons'):
-      for f in fs:
-        print os.path.basename(f)
-        files.append('icons/'+os.path.basename(f))
-    print files
-
-    p["/opt/khteditor"] = files
+    
+    p["/usr/lib/python2.5/site-packages"] = files
 
     p.postinstall = """#!/bin/sh
-chmod +x /usr/bin/khteditor
-python -m compileall /home/opt/khteditor"""
+chmod +x /usr/bin/khteditor_launch.py
+python -m compileall /usr/lib/python2.5/site-packages/khteditor"""
 
     p.changelog="""First Release
 """
 
 #print p.generate(build_binary=False,build_src=True)
-print p.generate(build_binary=False,build_src=True)
+print p.generate(build_binary=True,build_src=True)
