91e0ba7a66e4978a036581816629eeb5e12d7f91
user <user@Nokia-N900.(none)>
2010-08-13 21:42:43 +0200

    Fix whitespaceremover

---
+++ khteditor/plugins/whitespaceremover.py
@@ -0,0 +1,51 @@
+from PyQt4.QtGui import QTextEdit,QTextCursor
+from PyQt4.QtCore import Qt
+from khteditor.plugins_api import Plugin
+import os
+import re
+
+class WhiteSpaceRemover_Plugin(Plugin):
+    capabilities = ['afterKeyPressEvent','beforeFileSave']
+
+    def do_afterKeyPressEvent(self, widget,event):
+        if (event.key() == Qt.Key_Return) or (event.key() == Qt.Key_Enter):
+            # delete whitespace at end of the previous line
+            cursor = widget.textCursor()
+            line_number = widget.document().findBlock( cursor.position()).blockNumber()
+            cursor.beginEditBlock()
+            cursor.movePosition(QTextCursor.PreviousBlock,QTextCursor.KeepAnchor)
+            cursor.select(QTextCursor.BlockUnderCursor)
+            text = unicode.rstrip(unicode(cursor.selectedText()),' \t')
+            cursor.removeSelectedText()
+            cursor.insertText(text)
+            cursor.setPosition(widget.document().findBlockByLineNumber(line_number).position())
+            widget.setTextCursor(cursor)
+            
+    def do_beforeFileSave(self, widget):
+        if (event.key() == Qt.Key_Return) or (event.key() == Qt.Key_Enter):
+            # delete whitespace at end of the previous line
+            cursor = widget.textCursor()
+            line_number = widget.document().findBlock( cursor.position()).blockNumber()
+            cursor.beginEditBlock()
+            cursor.select(QTextCursor.Document)
+            text = cursor.selectedText()
+            new_text = u''
+            for line in text.splitlines():
+                new_text += '%s%s' % (unicode.rstrip(unicode(line),' \t'),os.linesep)
+            
+            text = unicode.rstrip(unicode(cursor.selectedText()),' \t')
+            cursor.removeSelectedText()
+            cursor.insertText(text)
+            cursor.setPosition(widget.document().findBlockByLineNumber(line_number).position())
+            widget.setTextCursor(cursor)
+            
+#            block = widget.document().findBlockByNumber(cursor.blockNumber()-1)
+#            cursor.select(QTextCursor.Document)            
+#            cursor.select(block)            
+#            text = unicode.lstrip(unicode(cursor.selectedText()),' \t')
+#            cursor.removeSelectedText()
+#            text = text.replace(unichr(8233), u'\n')
+#            cursor.insertText(text)
+#            cursor.setPosition(widget.document().findBlockByLineNumber(line_number).position())
+#            widget.setTextCursor(cursor)
+            cursor.endEditBlock()
