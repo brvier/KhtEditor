38f1f0142553b9e98f12f874f2d228cf650003ea
user <user@Nokia-N900.(none)>
2010-09-06 17:59:57 +0200

    Fix make and clean up scheme

--- make.py
+++ make.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
 chmod +x /usr/bin/khteditor_launch.py
 python -m compileall /usr/lib/python2.5/site-packages/khteditor"""
 
-    p.changelog="""Minor bug fix, improve pylint plugin
+    p.changelog="""Fix pylint plugin, optimize hilighters and add several native hilighter.
 """
 
 print p.generate(build_binary=False,build_src=True)
